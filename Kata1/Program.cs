c19818e357d55d4146089887d738465b623f96ac
Brajjaan <bryanvouriot@hotmail.com>
2024-11-23 18:42:28 +0100

    1.1

--- Kata1/Program.cs
+++ Kata1/Program.cs
@@ -4,28 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Character warrior = new("Warrior", 45);
-        Character healer = new("Healer", 55);
+        Character warrior = new("Warrior", 55);
+        Character healer = new("Healer", 45);
         
         warrior.PrimaryAction = () => Console.WriteLine("Warrior is attacking!");
 
         healer.PrimaryAction = () =>
         {
-            //Ternary operator to decide who recieves healing
+            //Ternary operator to decide who receives healing
             Character target = (warrior.Health < healer.Health) ? warrior : healer;
-            int healAmount = 15;
+            int healAmount = 10;
             target.Health += healAmount;
             Console.WriteLine($"Healer heals {target.Name} for {healAmount} health");
             Console.WriteLine($"{target.Name} has {target.Health} health");
         };
         var characters = new[] { warrior, healer };
+        
+        Console.WriteLine($"{warrior.Name} has {warrior.Health} health");
+        Console.WriteLine($"{healer.Name} has {healer.Health} health");
+        Console.WriteLine();
 
-        for (int turn = 1; turn <= 3; turn++)
+        for (int turn = 1; turn <= 5; turn++)
         {
             Console.WriteLine("Turn " + turn);
-            foreach (var character in characters.OrderByDescending(character => character.Health < 50))
+            foreach (var character in characters.OrderByDescending(c => c.Health < 50).ThenByDescending(c => c.Health))
             {
                 character.PrimaryAction();
+            }
+            //This is to test that the < 50 condition works :)
+            if (turn % 2 == 0) 
+            {
+                warrior.Health -= 20;
+                Console.WriteLine($"Warrior takes 20 damage!");              
             }
         }
     }
