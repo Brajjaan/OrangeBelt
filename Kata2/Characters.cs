01faf59d9c3e329ba7fa8dcb6a17e87173aac4b4
Brajjaan <bryanvouriot@hotmail.com>
2024-11-24 18:45:37 +0100

    2

---
+++ Kata2/Characters.cs
@@ -0,0 +1,30 @@
+﻿namespace Kata2;
+
+public delegate void CharacterAction(Character target);
+
+public class Character
+{
+    public string Name { get; }
+    public int Health { get; private set; }
+
+    public event Action<Character> HealthChanged;
+
+    public Character(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+
+    public void Attack(Character target)
+    {
+        CharacterAction attackAction = target.TakeDamage;
+        attackAction.Invoke(target);
+    }
+
+    public void TakeDamage(Character target)
+    {
+        int damage = 10; 
+        target.Health -= damage;
+        target.HealthChanged.Invoke(target);
+    }
+}
---
+++ Kata2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Kata2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        static void PrintHealthChanged(Character character) // I think this is what was asked??
+        {
+            Console.WriteLine($"{character.Name}'s health is now {character.Health}.");
+        }
+        
+        Character knight = new Character("Knight", 100);
+        Character mage = new Character("Mage", 80);
+
+        knight.HealthChanged += PrintHealthChanged;
+        mage.HealthChanged += PrintHealthChanged;
+
+        Console.WriteLine($"{knight.Name} attacks {mage.Name}!");
+        knight.Attack(mage);
+
+        Console.WriteLine($"{mage.Name} attacks {knight.Name}!");
+        mage.Attack(knight);        
+    }
+}
